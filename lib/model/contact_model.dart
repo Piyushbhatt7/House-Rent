0954266e4d32015f8966ef801deb704c059f2ce7
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-08 23:56:15 +0530

    contact model file

---
+++ lib/model/contact_model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ContactModel {
+
+  String? id;
+  String? firstName;
+  String? lastName;
+  String? fullName;
+  MemoryImage? displayImage;
+
+  ContactModel ({
+      this.id = "",
+      this.firstName = "",
+      this.lastName = "",
+      this.displayImage,
+  });
+
+  String getFullNameofUser ()
+  {
+    return fullName = 
+  }
+}
