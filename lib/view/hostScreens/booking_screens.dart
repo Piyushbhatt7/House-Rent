efed824378460987d0c35aea59e2ee1dac324be4
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-11 13:55:19 +0530

    host home screen

---
+++ lib/view/hostScreens/booking_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookingScreens extends StatefulWidget {
+  const BookingScreens({super.key});
+
+  @override
+  State<BookingScreens> createState() => _BookingScreensState();
+}
+
+class _BookingScreensState extends State<BookingScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
