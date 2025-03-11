492716e20ed1da0df8e3ddc57e9fd3a6b8461ad0
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-11 11:08:03 +0530

    trips screens

---
+++ lib/view/guestScreens/trips_screens.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TripsScreens extends StatefulWidget {
+  const TripsScreens({super.key});
+
+  @override
+  State<TripsScreens> createState() => _TripsScreensState();
+}
+
+class _TripsScreensState extends State<TripsScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Explore screen"),
+      ),
+    );
+  }
+}
