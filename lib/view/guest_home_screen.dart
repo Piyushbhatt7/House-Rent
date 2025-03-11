a6d52c40417a0568ab3e229431e32d0fba1d7095
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-11 11:20:32 +0530

    guest home screen

--- lib/view/guest_home_screen.dart
+++ lib/view/guest_home_screen.dart
@@ -58,9 +58,9 @@ class _GuestHomeScreenState extends State<GuestHomeScreen> {
         [
           customNavigationBarItem(0, Icons.search_outlined, screenTitles[0]),
           customNavigationBarItem(1, Icons.favorite_border_outlined, screenTitles[1]),
-          customNavigationBarItem(2, Icons.search_outlined, screenTitles[2]),
-          customNavigationBarItem(3, Icons.search_outlined, screenTitles[3]),
-          customNavigationBarItem(4, Icons.search_outlined, screenTitles[4]),
+          customNavigationBarItem(2, Icons.hotel_class_outlined, screenTitles[2]),
+          customNavigationBarItem(3, Icons.message_outlined, screenTitles[3]),
+          customNavigationBarItem(4, Icons.person_outlined, screenTitles[4]),
         ]
         ),
     );
