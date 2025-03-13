794920e30b2b0ebc76624252ac2107495bf6d29a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-13 21:09:33 +0530

    creating posting to list our houses

--- lib/view_model/posting_view_model.dart
+++ lib/view_model/posting_view_model.dart
@@ -1,7 +1,11 @@
+import 'package:house/model/posting_model.dart';
+
 class PostingViewModel {
 
   addListingInfoToFirestore () async
   {
-    
+    PostingModel posting  = PostingModel();
+
+    Map<String, dynamic> dataMap = {};
   }
 }
