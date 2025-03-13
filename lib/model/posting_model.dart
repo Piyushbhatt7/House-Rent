cd092fdff541e6c0826b45b0ebf2043d6d28a713
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-13 12:06:10 +0530

    posting view model

---
+++ lib/model/posting_model.dart
@@ -0,0 +1,13 @@
+class PostingModel 
+{
+   String? id;
+   String? name;
+   String? type;
+   double? price;
+   String? description;
+   String? address;
+   String? city;
+   String? country;
+   double? rating;
+
+}
