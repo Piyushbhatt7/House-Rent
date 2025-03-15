a221d70f6abe7560ef17864b6e551b2cb670261f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-15 21:55:34 +0530

    listing user posting

--- lib/view/widgets/posting_grid_tile_ui.dart
+++ lib/view/widgets/posting_grid_tile_ui.dart
@@ -63,7 +63,7 @@ class _PostingGridTileUiState extends State<PostingGridTileUi>
           ),
 
           Text(
-            '\₹ ${posting!.price} / month',
+            '\₹${posting!.price} / month',
             style: const TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w500,
