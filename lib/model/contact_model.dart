599ab18b5d50965e43b5081784c8bce5135bd2e1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-16 14:53:44 +0530

    contact model

--- lib/model/contact_model.dart
+++ lib/model/contact_model.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:house/model/user_model.dart';
 
@@ -48,5 +50,8 @@ class ContactModel {
       return displayImage!;
 
     }
+
+    final imageData = await FirebaseStorage.instance.ref()
+    .child("userImages")
   }
 }  
