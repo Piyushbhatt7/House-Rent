4a17cc6982e76c91eac1fe8ef3a361f5cd8f7efa
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-16 22:53:50 +0530

    posting info page

---
+++ lib/view/widgets/posting_info_tile_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostingInfoTileUi extends StatefulWidget {
+  const PostingInfoTileUi({super.key});
+
+  @override
+  State<PostingInfoTileUi> createState() => _PostingInfoTileUiState();
+}
+
+class _PostingInfoTileUiState extends State<PostingInfoTileUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
