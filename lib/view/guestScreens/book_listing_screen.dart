6f1a17eb28881ac57ebd5935268fbc354496df1e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-17 23:16:14 +0530

    booking screen

--- lib/view/guestScreens/book_listing_screen.dart
+++ lib/view/guestScreens/book_listing_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:house/model/posting_model.dart';
+import 'package:house/view/widgets/posting_grid_tile_ui.dart';
 
-class BookListingScreen extends StatefulWidget {
-  const BookListingScreen({super.key});
+class BookListingScreen extends StatefulWidget
+ {
+  PostingModel? posting;
+
+  
+   BookListingScreen({super.key, this.posting});
 
   @override
   State<BookListingScreen> createState() => _BookListingScreenState();
