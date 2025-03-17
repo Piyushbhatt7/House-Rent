d57f7788dd1182a171697aadc3bf2ddb8a065fcf
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-17 23:15:01 +0530

    booking screen

---
+++ lib/view/guestScreens/book_listing_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookListingScreen extends StatefulWidget {
+  const BookListingScreen({super.key});
+
+  @override
+  State<BookListingScreen> createState() => _BookListingScreenState();
+}
+
+class _BookListingScreenState extends State<BookListingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
