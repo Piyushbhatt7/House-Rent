015ee1a7d928815e90186bfc977598e4da957ad9
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-18 10:07:59 +0530

    calender ui

---
+++ lib/view/widgets/calender_ui.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CalenderUi extends StatefulWidget {
+
+  int? monthIndex;
+  List<DateTime>? bookedDates;
+  Function? selectDates;
+  Function? getSelectedDates;
+  
+  CalenderUi({super.key});
+
+  @override
+  State<CalenderUi> createState() => _CalenderUiState();
+}
+
+class _CalenderUiState extends State<CalenderUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
