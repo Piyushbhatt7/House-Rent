f7955d1ab1f735b6283b2e3ccb216a009ebfbe5e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-18 10:20:30 +0530

    app Constants

--- lib/model/app_constants.dart
+++ lib/model/app_constants.dart
@@ -30,4 +30,20 @@ class AppConstants
      11: 30,
      12: 31
    };
+
+   static final Map<int, String> monthDict = {
+
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+   };
 }
