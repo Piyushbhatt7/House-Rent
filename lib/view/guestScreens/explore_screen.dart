d9dff3c7d4888c8416d67df270dc661634367d74
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-19 20:09:28 +0530

    explore screen

--- lib/view/guestScreens/explore_screen.dart
+++ lib/view/guestScreens/explore_screen.dart
@@ -1,10 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:house/model/posting_model.dart';
 import 'package:house/view/view_posting_screen.dart';
 import 'package:house/view/widgets/posting_grid_tile_ui.dart';
+
 
 class ExploreScreen extends StatefulWidget {
   const ExploreScreen({super.key});
@@ -14,7 +15,6 @@ class ExploreScreen extends StatefulWidget {
 }
 
 class _ExploreScreenState extends State<ExploreScreen> {
-
   TextEditingController controllerSearch = TextEditingController();
   Stream stream = FirebaseFirestore.instance.collection('postings').snapshots();
   String searhType = "";
@@ -23,19 +23,14 @@ class _ExploreScreenState extends State<ExploreScreen> {
   bool isCityButtonSelected = false;
   bool isTypeButtonSelected = false;
 
-  searchByField()
-  {
-   
-  }
+  searchByField() {}
 
-  pressSerachByButton(String searchTypeStr, bool isNameButtonSelectedB, bool isCityButtonSelectedB, bool isTypeButtonSelectedB)
-  {
+  pressSerachByButton(String searchTypeStr, bool isNameButtonSelectedB, bool isCityButtonSelectedB, bool isTypeButtonSelectedB) {
     searhType = searchTypeStr;
     isNameButtonSelected = isNameButtonSelectedB;
     isCityButtonSelected = isCityButtonSelectedB;
     isTypeButtonSelected = isTypeButtonSelectedB;
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -44,24 +39,20 @@ class _ExploreScreenState extends State<ExploreScreen> {
       child: SingleChildScrollView(
         child: Column(
           children: [
-
             // searchBar
-
             Padding(
               padding: EdgeInsets.only(top: 0, bottom: 0),
               child: TextField(
                 decoration: const InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey,
-                      width: 20.0,
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                        width: 20.0,
+                      ),
                     ),
-                  ),
-
-                  contentPadding: EdgeInsets.all(5.0)
-                ),
+                    contentPadding: EdgeInsets.all(5.0)),
                 style: const TextStyle(
                   fontSize: 20.0,
                   color: Colors.black,
@@ -72,7 +63,6 @@ class _ExploreScreenState extends State<ExploreScreen> {
             ),
 
             // name - city - type - clear button
-
             SizedBox(
               height: 48,
               width: MediaQuery.of(context).size.width / .4,
@@ -80,106 +70,88 @@ class _ExploreScreenState extends State<ExploreScreen> {
                 scrollDirection: Axis.horizontal,
                 padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
                 children: [
-
-                  MaterialButton(onPressed: ()
-                  {
-                    pressSerachByButton("name", true, false, false);
-                  },
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                  color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
-                  child: const Text("Name"),
+                  MaterialButton(
+                    onPressed: () {
+                      pressSerachByButton("name", true, false, false);
+                    },
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                    color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
+                    child: const Text("Name"),
                   ),
-
-                  const SizedBox(width: 5.0,),
-
-                  MaterialButton(onPressed: ()
-                  {
-                    pressSerachByButton("city", false, true, false);
-                  },
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                  color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
-                  child: const Text("City"),
+                  const SizedBox(width: 5.0),
+                  MaterialButton(
+                    onPressed: () {
+                      pressSerachByButton("city", false, true, false);
+                    },
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                    color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
+                    child: const Text("City"),
                   ),
-
-                   const SizedBox(width: 5.0,),
-
-                  MaterialButton(onPressed: ()
-                  {
-                    pressSerachByButton("type", false, false, true);
-                  },
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                  color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
-                  child: const Text("Type"),
+                  const SizedBox(width: 5.0),
+                  MaterialButton(
+                    onPressed: () {
+                      pressSerachByButton("type", false, false, true);
+                    },
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                    color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
+                    child: const Text("Type"),
                   ),
-
-                   const SizedBox(width: 5.0,),
-
-                  MaterialButton(onPressed: ()
-                  {
-                    pressSerachByButton("", false, false, false);
-                  },
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                  color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 10.0),
-                    child: const Text("Clear"),
+                  const SizedBox(width: 5.0),
+                  MaterialButton(
+                    onPressed: () {
+                      pressSerachByButton("", false, false, false);
+                    },
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                    color: isNameButtonSelected ? Colors.pinkAccent : Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: const Text("Clear"),
+                    ),
                   ),
-                  ),
-                  
                 ],
               ),
             ),
-         
-          // display listing
 
-          StreamBuilder(
-            stream: stream, 
-            builder: (context, dataSnapshots)
-            {
-              if(dataSnapshots.hasData)
-              {
-                return GridView.builder(
-                  physics: const ScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: dataSnapshots.data.docs.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 15,
-                    childAspectRatio: 3 / 4,
-                    ), 
-                  itemBuilder: (context, index)
-                  {
-                     DocumentSnapshot snapshot = dataSnapshots.data.docs[index];
-
-                     PostingModel cPosting  = PostingModel(id: snapshot.id);
-
-                     cPosting.getPostingInfoFromSnapshot(snapshot);
-
-                     return InkResponse(
-                      onTap: () {
-                        Get.to(ViewPostingScreen(posting: cPosting,));
-                      },
-                      enableFeedback: true,
-                      child: PostingGridTileUi(
-                        posting: cPosting,
-                      ),
-                     );
+            // display listing
+            StreamBuilder(
+                stream: stream,
+                builder: (context, dataSnapshots) {
+                  if (dataSnapshots.hasData) {
+                    return GridView.builder(
+                        physics: const ScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: dataSnapshots.data.docs.length,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 15,
+                          childAspectRatio: 3 / 4,
+                        ),
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot snapshot = dataSnapshots.data.docs[index];
+                          PostingModel cPosting = PostingModel(id: snapshot.id);
+                          cPosting.getPostingInfoFromSnapshot(snapshot);
+                          return InkResponse(
+                            onTap: () {
+                              Get.to(ViewPostingScreen(
+                                posting: cPosting,
+                              ));
+                            },
+                            enableFeedback: true,
+                            child: PostingGridTileUi(
+                              posting: cPosting,
+                            ),
+                          );
+                        });
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
                   }
-                  );
-              }
-
-              else{
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }
-            )
-         
+                })
           ],
         ),
       ),
-      );
+    );
   }
 }
