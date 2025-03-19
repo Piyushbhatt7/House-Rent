3de925533fa4339f3a4a45ec23b41b52f5ce5711
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-19 20:09:40 +0530

    user profile

---
+++ lib/model/user_profile.dart
@@ -0,0 +1,28 @@
+class FetchImagesNetwork {
+  static List<Map<String, String>> houseListings = [
+    {
+      "image": "https://source.unsplash.com/random/400x300/?house,home",
+      "title": "Modern House in LA"
+    },
+    {
+      "image": "https://source.unsplash.com/random/400x300/?villa,mansion",
+      "title": "Luxury Villa in Miami"
+    },
+    {
+      "image": "https://source.unsplash.com/random/400x300/?apartment,building",
+      "title": "Cozy Apartment in NYC"
+    },
+    {
+      "image": "https://source.unsplash.com/random/400x300/?cottage,woodenhouse",
+      "title": "Beautiful Cottage in Canada"
+    },
+    {
+      "image": "https://source.unsplash.com/random/400x300/?bungalow,house",
+      "title": "Beachside Bungalow in Goa"
+    },
+    {
+      "image": "https://source.unsplash.com/random/400x300/?architecture,home",
+      "title": "Elegant Architecture Home"
+    }
+  ];
+}
