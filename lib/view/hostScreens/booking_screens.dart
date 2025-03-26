a31007f2cef54d3ed6cc6f801a438de0e09a1260
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-26 23:00:31 +0530

    booking screen

--- lib/view/hostScreens/booking_screens.dart
+++ lib/view/hostScreens/booking_screens.dart
@@ -91,6 +91,7 @@ class _BookingScreensState extends State<BookingScreens> {
                     return CalenderUi(
                       monthIndex: index,
                       bookedDates: _bookedDates,
+                      selectDates: ,
                     )
                   }),
               ),
