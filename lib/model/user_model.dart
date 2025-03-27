b16c3bf8cea0fd9ab61972876e6d120747cdd828
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-27 22:03:44 +0530

    adding messaging feature

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -196,7 +196,7 @@ class UserModel extends ContactModel
       ConversationModel conversation = ConversationModel();
       conversation.addConversationToFirestore(booking.posting!.host!);
 
-      String textMessage = "Hi my name is ${AppConstants.currentUser!.firstName} and I have "
+      String textMessage = "Hi my name is ${AppConstants.currentUser.firstName} and I have "
       "just booked ${booking.posting!.name} from ${booking.dates!.first} to "
       "${booking.dates!.last} if you have any questions contact me. Enjoy your "
       "stay!";
