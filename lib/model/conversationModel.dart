fee5e3fad014b3f894d56787c1bdc7c7c73d7e14
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-27 18:16:26 +0530

    adding messaging feature

---
+++ lib/model/conversationModel.dart
@@ -0,0 +1,11 @@
+import 'package:house/model/contact_model.dart';
+import 'package:house/model/message_model.dart';
+
+class ConversationModel {
+
+  String? id;
+  ContactModel? contact;
+  List<MessageModel>? message;
+  MessageModel? lastMessage;
+
+}
