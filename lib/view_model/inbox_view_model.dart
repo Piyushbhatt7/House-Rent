1d589455b2c0c8b7a54cf9dabb6abcae77eb91c8
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-28 19:35:50 +0530

    inbox screen

---
+++ lib/view_model/inbox_view_model.dart
@@ -0,0 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class InboxViewModel {
+
+  getConversations()
+  {
+    return FirebaseFirestore.instance
+  }
+}
