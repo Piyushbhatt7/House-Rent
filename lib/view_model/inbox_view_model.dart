7443916b4849eacd58e2e97a8fe02b42d3d924e9
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-28 19:36:37 +0530

    inbox screen

--- lib/view_model/inbox_view_model.dart
+++ lib/view_model/inbox_view_model.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:house/model/app_constants.dart';
 
 class InboxViewModel {
 
   getConversations()
   {
     return FirebaseFirestore.instance
+    .collection('conversations')
+    .where('userIDs', arrayContains: AppConstants.currentUser.id)
   }
 }
