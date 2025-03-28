6eb4193ee450a9e7e27fbc0b9c0818babd4d18fc
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-28 22:59:45 +0530

    inbox screen

---
+++ lib/view/widgets/conversation_list_tile_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConversationListTileUi extends StatefulWidget {
+  const ConversationListTileUi({super.key});
+
+  @override
+  State<ConversationListTileUi> createState() => _ConversationListTileUiState();
+}
+
+class _ConversationListTileUiState extends State<ConversationListTileUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
