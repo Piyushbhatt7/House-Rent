559335246393ccaa5cfd0accaaaa4c8d13a08eff
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-29 21:40:17 +0530

    conversation screen

--- lib/view/conversation_screen.dart
+++ lib/view/conversation_screen.dart
@@ -1,1 +1,13 @@
-state
+class ConversationScreen extends StatefulWidget {
+  const ConversationScreen({super.key});
+
+  @override
+  State<ConversationScreen> createState() => _ConversationScreenState();
+}
+
+class _ConversationScreenState extends State<ConversationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
