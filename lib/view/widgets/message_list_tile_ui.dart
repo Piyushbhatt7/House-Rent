4baabdb8a2cc7795ca0b5d51800973f3b093d020
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-30 00:25:51 +0530

    message list tile

--- lib/view/widgets/message_list_tile_ui.dart
+++ lib/view/widgets/message_list_tile_ui.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:house/model/message_model.dart';
 
 class MessageListTileUi extends StatelessWidget {
-  const MessageListTileUi({super.key});
+
+  MessageModel? message;
+  MessageListTileUi({super.key});
 
   @override
   Widget build(BuildContext context) {
