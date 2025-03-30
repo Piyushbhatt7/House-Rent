6d6e89ebfb53bb295e960a5eceb9ef99f98dffe5
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-30 20:00:36 +0530

    displaying messages to user screen

--- lib/view/widgets/message_list_tile_ui.dart
+++ lib/view/widgets/message_list_tile_ui.dart
@@ -11,7 +11,68 @@ class MessageListTileUi extends StatelessWidget {
   Widget build(BuildContext context) {
     if(message!.sender!.firstName == AppConstants.currentUser.firstName) 
     {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 16, 36, 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+
+        
+
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 11),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [
+                      Colors.lightGreenAccent,
+                      Colors.purpleAccent,
+                    ],
+                    begin: FractionalOffset(0.0, 0.0),
+                    end: FractionalOffset(1.0, 0.0),
+                    stops: [0.0, 1.0],
+                    tileMode: TileMode.clamp
+                    ),
+                    borderRadius: BorderRadius.circular(10) // 11:17 - 25
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        message!.text!,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                        textWidthBasis: TextWidthBasis.parent,
+                      ),
+                    
+                    ),
+
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        message!.getMessageDateTime(),
+                        style: const TextStyle(
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+               )
+          )
+        ],
+      ),
+      );
+ 
   }
   else {
     return Padding(
@@ -83,6 +144,7 @@ class MessageListTileUi extends StatelessWidget {
         ],
       ),
       );
+ 
   }
   }
 }
