e181516809ba34b5a4a1370a783963f3b25231a7
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-04 23:04:35 +0530

    creating posting screen

--- lib/view/hostScreens/create_posting_screens.dart
+++ lib/view/hostScreens/create_posting_screens.dart
@@ -300,7 +300,7 @@ class _CreatePostingScreensState extends State<CreatePostingScreens> {
                         ),
                       ),
                       ),
-                    ),
+                    ), 
                     
                     // Price
                   
